e868951ab96d32f38aae34013b6274ef1b56c8a1
Cheng Cheng <yushen740@gmail.com>
2024-10-11 17:57:00 +0200

    examen

---
+++ Examen_ChengCheng/examen.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tablero de damas</title>
+</head>
+<body>
+    <h1>Tablero de damas</h1>    
+    <?php
+        $tablero = 8;
+
+        for ($i = 0; $i < $tablero; $i++) {
+            echo "<svg width='100' height='100' xmlns='http://www.w3.org/2000/svg'>";
+            $color = ($i % 2 == 0) ? 'white' : 'black';
+            for ($j = 0; $j < $tablero; $j++) {
+                $x1 = $j/10;
+                $y1 = $i/10;
+                
+                echo "<rect x='$x1' y='$y1' width='100' height='100' fill='$color'/>";
+            }
+            echo "</svg>";
+        } 
+    ?>
+</body>
+</html>
