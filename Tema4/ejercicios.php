4c5280433efb537abeb60d930bdd5e819c62797c
Cheng Cheng <yushen740@gmail.com>
2024-10-14 21:38:42 +0200

    cambio

--- Tema4/ejercicios.php
+++ Tema4/ejercicios.php
@@ -13,9 +13,9 @@ if (function_exists('esPrimo')) {
 $numero = 29;
 echo "\n<h3>Ejercicio 2:</h3>";
 if (esPrimo($numero)) {
-    echo "<p>- $numero es primo.</p>";
+    echo "\n\t<p>- $numero es primo.</p>";
 } else {
-    echo "<p>- $numero no es primo.</p>";
+    echo "\n\t<p>- $numero no es primo.</p>";
 }
 
 //Ejercicio 3.
@@ -23,14 +23,14 @@ echo "\n<h3>Ejercicio 3:</h3>";
 
 $n1 = 10;
 $n2 = 40;
-echo "<p>- Los números primos entre $n1 y $n2 son: " . primosEntre($n1, $n2) ."</p>";
+echo "\n\t<p>- Los números primos entre $n1 y $n2 son: " . primosEntre($n1, $n2) ."</p>";
 
 //Ejercicio 4.
 echo "\n<h3>Ejercicio 4:</h3>";
 
 $n1 = 10;
 $n2 = 20;
-echo "<p>- La suma de los números primos entre $n1 y $n2 es: " . sumaPrimos($n1, $n2)."</p>";
+echo "\n\t<p>- La suma de los números primos entre $n1 y $n2 es: " . sumaPrimos($n1, $n2)."</p>";
 
 //Ejercicio 5.
 echo "\n<h3>Ejercicio 5:</h3>";
@@ -39,16 +39,16 @@ $numero1 = 34;
 $numero2 = 2;
 
 if (esDivisor($numero1, $numero2)) {
-    echo "<p>- $numero2 es divisor de $numero1.</p>";
+    echo "\n\t<p>- $numero2 es divisor de $numero1.</p>";
 } else {
-    echo "<p>- $numero2 no es divisor de $numero1.</p>";
+    echo "\n\t<p>- $numero2 no es divisor de $numero1.</p>";
 }
 
 //Ejercicio 6.
 echo "\n<h3>Ejercicio 6:</h3>";
 
 $numero=72;
-echo "<p>- Los divisores de $numero son: " . obtenerDivisores($numero). "</p>";
+echo "\n\t<p>- Los divisores de $numero son: " . obtenerDivisores($numero). "</p>";
 
 //Ejercicio 7.
 echo "\n<h3>Ejercicio 7:</h3>";
@@ -61,13 +61,14 @@ $media = 0;
 calcularSumaYMedia($suma, $media, 4, 5, 6, 7, 8);
 
 // Imprimir los resultados
-echo "<p>- La suma de los números es: $suma</p>";
-echo "<p>- La media de los números es: $media</p>";
+echo "\n\t<p>- La suma de los números es: $suma</p>";
+echo "\n\t<p>- La media de los números es: $media</p>";
 
 //Ejercicio 10.
 echo "\n<h3>Ejercicio 10:</h3>";
 
 $numero = 250;
+echo "\n\t-";
 echo divisoresPrimos($numero);
 
 //Ejercicio 12.
@@ -75,7 +76,7 @@ echo "\n<h3>Ejercicio 12:</h3>";
 
 // Ejemplo de uso
 $n = 255; // Cambiar este número para probar con otros valores
-echo "- El número binario de $n es: " . decimalABinario($n);
+echo "\n\t- El número binario de $n es: " . decimalABinario($n);
 
 
 ?>
