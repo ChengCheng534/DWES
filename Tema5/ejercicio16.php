6aa995e78967db946cd2d4b54cf80035321b6ac0
Cheng Cheng <yushen740@gmail.com>
2024-11-08 21:36:48 +0100

    ej

--- Tema5/ejercicio16.php
+++ Tema5/ejercicio16.php
@@ -10,22 +10,70 @@
     $cartasJug1 = array();
     $cartasJug2 = array();
     $cartasMesa = array();
+
     $jugador = 1;
     $contador = 0;
     
-    function iguales($array){
-        if ($array[0]===$array[1]) {
-            return $array[0];
+    function parejasJug($array){
+        $vistos = [];
+        $duplicados = [];
+
+        foreach ($array as $numero) {
+            if (isset($vistos[$numero])) {
+                $duplicados[$numero] = true;
+            } else {
+                $vistos[$numero] = true;
+            }
+        }
+        if ($duplicados == null){
+            echo 0;
         }else{
-            return 0;
+            echo implode(', ', array_keys($duplicados));
         }
     }
 
-    function parejas($array1, $array2){
-        $igual = array_intersect($array1, $array2);
-        return $igual;
+    function coincidirMesa($cartaJug, $cartaMesa){
+        $igual = array_intersect($cartaJug, $cartaMesa);
+        if ($igual == null) {
+            return 0;
+        }else{
+            $resultado = array_merge($array1, $array2);
+            return $resultado;
+        }
     }
 
+    function contarCartas($array){
+        $contador = [];
+        foreach ($array as $numero) {
+            if (isset($contador[$numero])) {
+                $contador[$numero]++;
+            } else {
+                $contador[$numero] = 1;
+            }
+        }
+
+        foreach ($contador as $numero => $cantidad) {
+            if ($cantidad > 1) {
+                echo "<p>El número $numero se repite $cantidad veces.</p><br>";
+            }elseif($cantidad > 2){
+                echo "<p>El número $numero se repite $cantidad veces.</p><br>";
+            }elseif($cantidad > 3){
+                echo "<p>El número $numero se repite $cantidad veces.</p><br>";
+            }elseif($cantidad > 4){
+                echo "<p>El número $numero se repite $cantidad veces.</p><br>";
+            }
+        }
+    }
+
+    function resultado($cartasJug1, $cartasJug2){
+        if (parejas($cartasJug1) > parejas($cartasJug2)) {
+            echo "<p>Resultado: Gana el jugador 1</p>";
+        }elseif(parejas($cartasJug1) < parejas($cartasJug2)){
+            echo "<p>Resultado: Gana el jugador 2</p>";
+        }else{
+            echo "<p>Resultado: empate.</p>";
+        }
+    }
 
     while ($jugador <= 2) {
         echo "\n<p>Jugador ".$jugador."</p>\n";
@@ -33,13 +81,13 @@
         if ($jugador == 1) {
             for ($i=0; $i < 2; $i++) { 
                 $carta = rand(1,10);
-                echo "<img src='../Tema3/cartas/c$carta.svg' alt=''>\n";
+                echo "<img src='../Tema3/cartas/c$carta.svg' alt='' width='120'>\n";
                 $cartasJug1[] = $carta;
             }
         }else{
             for ($i=0; $i < 2; $i++) { 
                 $carta = rand(1,10);
-                echo "<img src='../Tema3/cartas/c$carta.svg' alt=''>\n";
+                echo "<img src='../Tema3/cartas/c$carta.svg' alt='' width='120'>\n";
                 $cartasJug2[] = $carta;
             }
         }
@@ -48,24 +96,17 @@
     echo "\n<p>Carta mesa:</p>\n";
     for ($i=0; $i < 3; $i++) { 
         $carta = rand(1,10);
-        echo "<img src='../Tema3/cartas/c$carta.svg' alt=''>\n";
+        echo "<img src='../Tema3/cartas/c$carta.svg' alt='' width='120'>\n";
         $cartasMesa[] = $carta;
     }
 
-    if (iguales($cartasJug1) > iguales($cartasJug2)) {
-        echo "\nGano el jugador 1";
-    }elseif(iguales($cartasJug1) < iguales($cartasJug2)){
-        echo "\nGano el jugador 2";
-    }else{
-        echo "\nEmpate";
-    }
-    //print_r(parejas($cartasJug1, $cartasMesa));
-    //print_r(parejas($cartasJug2, $cartasMesa));
-    //print_r($cartasJug2);
-    echo "<br>";
-    //print_r(iguales($cartasJug1));
-    echo "<br>";
-    //print_r(iguales($cartasJug2));
+    //echo parejasJug($cartasJug1);
+    //echo parejasJug($cartasJug2);
+    
+    //$coincide =coincidirMesa($cartasJug1, $cartaMesa);
+    //echo $coincide;
+    //echo resultado($cartasJug1, $cartasJug2);
+    //echo contarCartas(coincidirMesa($cartasJug1, $cartasMesa));
 ?>
 </body>
 </html>
