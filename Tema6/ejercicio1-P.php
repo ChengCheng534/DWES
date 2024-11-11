ae9f7c8a0eb87b83ee89a862e286a740255d2213
Cheng Cheng <yushen740@gmail.com>
2024-11-11 21:43:00 +0100

    guardar

--- Tema6/ejercicio1-P.php
+++ Tema6/ejercicio1-P.php
@@ -1,16 +1,16 @@
 <?php
-    class Producto{
+    class Producto {
         public $nombre;
         public $precio;
         public $cantidad;
 
-        public function __construct($nombre){
+        public function __construct($nombre) {
             $this->nombre = $nombre;
             $this->precio = 0.00;
             $this->cantidad = 0;
         }
 
-        public function calcularSubtotal(){
+        public function calcularSubtotal() {
             return $this->precio * $this->cantidad;
         }
 
@@ -23,6 +23,7 @@
                 return $this->$propiedad;
             }
         }
+
         public function __set($propiedad, $valor) {
             if (property_exists($this, $propiedad)) {
                 $this->$propiedad = $valor;
@@ -30,12 +31,12 @@
         }
     }
 
-    class Factura{
+    class Factura {
         public $numeroFactura;
         public $nombreCliente;
         public array $productos;
 
-        public function __construct($numeroFactura, array $productos){
+        public function __construct($numeroFactura, array $productos) {
             $this->numeroFactura = $numeroFactura;
             $this->productos = $productos;
         }
@@ -48,54 +49,82 @@
                     return;
                 }
             }
-            //$this->productos[] = $producto;
             array_push($this->productos, $producto);
         }
 
-        private function calcularBaseImponible(){
+        private function calcularBaseImponible() {
             $baseImponible = 0;
             foreach ($this->productos as $producto) {
-                $baseImponible += $producto->calcularSubTotal();
+                $baseImponible += $producto->calcularSubtotal();
             }
             return $baseImponible;
         }
 
-        private function totalFactura(){
+        private function totalFactura() {
             $baseImponible = $this->calcularBaseImponible();
             $iva = $baseImponible * 0.21;
             return $baseImponible + $iva;
         }
 
-        public function mostrarArticulos(){
-            usort($this->productos, function($articulo1, $articulo2){
-                if ($articulo2->calcularSubtotal == $articulo1->calcularSubtotal) {
+        public function mostrarArticulos() {
+            usort($this->productos, function($articulo1, $articulo2) {
+                if ($articulo2->calcularSubtotal() == $articulo1->calcularSubtotal()) {
                     return strcmp($articulo1->nombre, $articulo2->nombre);
                 }
-                return $articulo2->calcularSubtotal <=> $articulo1->calcularSubtotal;
+                return $articulo2->calcularSubtotal() <=> $articulo1->calcularSubtotal();
             });
 
             foreach ($this->productos as $producto) {
-                echo "- ".$producto->nombre.":\n\t Precio: ".$producto->precio. 
-                "€\n\t Subtotal: $".$producto->calcularSubTotal()."€\n";
+                printf("%-20s %-10.2f %-10d %-10.2f\n", $producto->nombre, $producto->precio, $producto->cantidad, $producto->calcularSubtotal());
             }
         }
+        
+        public function imprimirFactura() {
+            printf("<table border='1'>
+                    <tr>
+                        <th>Producto</th>
+                        <th>Precio Unitaria</th>
+                        <th>Cantidad</th>
+                        <th>Subtotal</th>
+                    </tr>");
 
-        public function imprimirFactura(){
-            echo "<p>Factura número: ".$this->numeroFactura."\t\t\t\tCliente: ".$this->nombreCliente."</p>\n";
-            echo "Artículo\t\tPrecio\t\tCantidad\t\tSubtotal\n";
             foreach ($this->productos as $producto) {
-                echo $producto->nombre."\t\t\t\t";
-                echo $producto->precio."\t\t\t";
-                echo $producto->cantidad."\t\t\t";
-                echo $producto->calcularSubtotal()."\n";
-                
+                printf("<tr>
+                            <td>%'#-20s</td>
+                            <td>%'@10.2f</td>
+                            <td>%5d</td>
+                            <td>%10.2f</td>
+                        </tr>", $producto->nombre, 
+                                $producto->precio, 
+                                $producto->cantidad, 
+                                $producto->calcularSubtotal());
             }
-            echo "\n\t\t\t\t\t\t Base Imposible: \t".$this->calcularBaseImponible()."€";
-            echo "\n\t\t\t\t\t\t IVA 21%: \t\t".$this->calcularBaseImponible()*0.21."€";
-            echo "\n\t\t\t\t\t\t Total Factura: \t".$this->totalFactura()."€";
+            printf("<tr>
+                        <td colspan='1' align='right'>Base Imponible:</td>
+                        <td></td>
+                        <td></td>
+                        <td>%10.2f</td>
+                    </tr>", $this->calcularBaseImponible());
+            
+            printf("<tr>
+                        <td colspan='1' align='right'>IVA 21%%:</td>
+                        <td></td>
+                        <td></td>
+                        <td>%10.2f</td>
+                    </tr>", $this->calcularBaseImponible() * 0.21);
+            
+            printf("<tr>
+                        <td colspan='1' align='right'>Total Factura:</td>
+                        <td></td>
+                        <td></td>
+                        <td>%10.2f</td>
+                    </tr>", $this->totalFactura());
+            
+            echo "</table>";
         }
+        
     }
-
+    // Creación de productos
     $producto1 = new Producto("PC");
     $producto1->precio = 99.99;
     $producto1->cantidad = 2;
@@ -111,18 +140,16 @@
     $producto4 = new Producto("Teclado");
     $producto4->precio = 45;
     $producto4->cantidad = 1;
-    //echo $producto1->cacularSubtotal();
 
+    // Crear la factura
     $productos = array();
-    $factura1 = new Factura(123456,$productos);
+    $factura1 = new Factura(123456, $productos);
     $factura1->nombreCliente = "Cheng";
     $factura1->agregarProducto($producto1);
     $factura1->agregarProducto($producto2);
     $factura1->agregarProducto($producto3);
     $factura1->agregarProducto($producto4);
 
-    //echo $factura1->mostrarArticulos();
+    // Imprimir la factura con formato
     echo $factura1->imprimirFactura();
-
-
 ?>
